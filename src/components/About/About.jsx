663a28163c3b753feffe0f8c84feb9a2883c7145
Kamalpreet Kaur <bhatiasona86@gmail.com>
2025-11-03 19:27:35 +0530

    Updated About.jsx and constants.js

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -52,7 +52,7 @@ const About = () => {
 
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/16zglKVYzV7KzFAGC4ZULpfqOXRHEIsdZ/view?usp=drive_link"
+            href="https://drive.google.com/file/d/16zglKVYzV7KzFAGC4ZULpfqOXRHEIsdZ/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
